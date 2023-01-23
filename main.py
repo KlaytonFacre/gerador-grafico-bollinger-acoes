2b87c0935880d2ea5715abc60b68e555b69dfaa4
Klayton Facre <klayton.facre@hotmail.com>
2023-01-23 09:43:42 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+acao = input('Qual o código da ação a ser processada? ').upper()
+
+with open(f'./assets/dados/{acao}.txt', 'r') as arquivo_cotacao:
+    linhas = arquivo_cotacao.readlines()
+
+    pass
+
