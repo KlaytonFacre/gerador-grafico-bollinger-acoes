d7c4dd67262a5eb13d3f878121ac929b9870dbf9
Klayton Facre <klayton.facre@hotmail.com>
2023-01-23 10:41:16 -0300

    23 JAN 2023 as 10h41

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
+from datetime import date
+from openpyxl import Workbook
+
+
 acao = input('Qual o código da ação a ser processada? ').upper()
 
 with open(f'./assets/dados/{acao}.txt', 'r') as arquivo_cotacao:
     linhas = arquivo_cotacao.readlines()
+    linhas = [linha.removesuffix('\n').split(';') for linha in linhas]
 
-    pass
+workbook = Workbook()
+planilha_ativa = workbook.active
+planilha_ativa.title = 'Dados'
 
+planilha_ativa.append(["DATA", "COTAÇÃO", "BANDA INFERIOR", "BANDA SUPERIOR"])
+
+indice = 2
+for linha in linhas:
+    # TRATANDO E PREPARANDO A DATA, CRIANDO UM OBJETO DATE DO PYTHON
+    ano_mes_dia = linha[0].split(" ")[0]
+    data = date(
+        year=int(ano_mes_dia.split("-")[0]),
+        month=int(ano_mes_dia.split("-")[1]),
+        day=int(ano_mes_dia.split("-")[2])
+    )
+
+    # TRATANDO E PREPARANDO A COTAÇÃO
+    cotacao = float(linha[1])
+
+    # ESCREVENDO DADOS NA PLANILHA ATIVA
+    # Data
+    planilha_ativa[f'A{indice}'] = data
+    # Cotação
+    planilha_ativa[f'B{indice}'] = cotacao
+    # Banda inferior de Bollinger
+    planilha_ativa[f'C{indice}'] = f'=AVERAGE(B{indice}:B{indice + 19}) - 2*STDEV(B{indice}:B{indice + 19})'
+    # Banda superior de Bollinger
+    planilha_ativa[f'D{indice}'] = f'=AVERAGE(B{indice}:B{indice + 19}) + 2*STDEV(B{indice}:B{indice + 19})'
+
+    # PREPARA PARA A ESCRITA NA PRÓXIMA LINHA
+    indice += 1
+
+
+workbook.save('./saida/Planilha.xlsx')
